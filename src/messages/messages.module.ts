78866fcfc1e5c36399aa7fbb7cf015b55f82b17e
saiashish9 <saiashish3760@gmail.com>
2021-02-09 02:25:23 +0530

    database integrated

--- src/messages/messages.module.ts
+++ src/messages/messages.module.ts
@@ -2,6 +2,7 @@ import { Module } from '@nestjs/common';
 import { MessagesResolver } from './messages.resolvers';
 
 @Module({
+  
   providers: [MessagesResolver],
   exports: [MessagesResolver],
 })
